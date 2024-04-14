efb2eb1a2f0079b2962d1a5d1fba3c6729d50800
Davi <davirobergemachado@gmail.com>
2024-04-14 15:30:52 -0300

    mais exercicios

---
+++ exercicios pra p1/1087.py
@@ -0,0 +1,13 @@
+end = 0
+
+while (end == 0):
+    x1, y1, x2, y2 = map(int, input("Digite as coordenadas inciais e as coordenadas finais: ").split())
+    if (x1+y1+x2+y2 == 0):
+        end = 1
+    else:
+        if (x1 == x2) and (y1 == y2):
+            print('0')
+        elif (x1 == x2) or (y1 == y2) or (x1+y1 == x2+y2) or (x1-y1 == x2-y2):
+            print('1')
+        else:
+            print('2')
--- exercicios pra p1/1306.py
+++ exercicios pra p1/1306.py
@@ -1,13 +1,26 @@
-end = 0
+def interpreta (r, n):
+  if(r <= n):
+    return "0"
 
-while end == 0:
-    x1, y1, x2, y2 = map(int, input("Digite as coordenadas inciais e as coordenadas finais: ").split())
-    if (x1+y1+x2+y2 == 0):
-        end = 1
-    else:
-        if (x1 == x2) and (y1 == y2):
-            print('0')
-        elif (x1 == x2) or (y1 == y2) or (x1+y1 == x2+y2) or (x1-y1 == x2-y2):
-            print('1')
-        else:
-            print('2')
+  elif(n*27 < r):
+    return "impossible"
+
+  else:
+    d = 1
+    while(n*d < r):
+      d += 1
+
+    return d-1
+
+end = 0
+case = 0
+
+while (end == 0):
+  case += 1
+  r, n = map(int, input("Digite a quantidade de ruas e quantos inteiros estão disponíveis: ").split())
+  if((r == 0) and (n == 0)):
+    end = 1
+
+  print(f"Case {case}: {interpreta(r, n)}")
+
+  
